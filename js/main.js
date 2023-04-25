ee7efc3d01b11d1da34a49e83243cff020e23172
Ricardo <ric.asjunior@gmail.com>
2023-04-25 12:01:46 -0300

    Criando botão de deletar elementos.

--- js/main.js
+++ js/main.js
@@ -24,11 +24,9 @@ form.addEventListener("submit", (evento) => {
 
         atualizaElemento(itemAtual);
 
-        itens[existe.id] = itemAtual;
-
-
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
     }else{
-        itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length - 1]).id + 1 : 0;
 
         criaElemento(itemAtual);
 
@@ -59,8 +57,29 @@ function criaElemento(item){
     
     lista.appendChild(novoItem);
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
 }
 
 function atualizaElemento(item){
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "x";
+
+    elementoBotao.addEventListener("click",  function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
